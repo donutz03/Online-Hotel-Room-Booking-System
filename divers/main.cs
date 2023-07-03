6fbd210bf3073707f1b393dd6728694f79a0b09a
masterofcsharp <109514731+masterofcsharp@users.noreply.github.com>
2023-07-03 17:49:47 +0000

    divers

---
+++ divers/main.cs
@@ -0,0 +1,96 @@
+
+    public class MyClass {
+int numar = 10;
+string nume = "John";
+bool isTrue = true;
+
+if (numar > 5) {
+    Console.WriteLine("Nr e mai mare decat 5");
+}
+
+for (int i = 0; i < 5; i++) 
+    Console.WriteLine(i);
+
+while (numar > 0)
+    {
+        Console.WriteLine(numar);
+        numar --;
+    }
+
+
+    }
+
+
+class Persoana
+{
+    public string nume;
+
+    public void Salut() 
+    {
+        Console.WriteLine("Salut, numele meu este " + nume + ".");
+    }
+}
+
+Persoana pers1 = new Persoana();
+
+pers1.nume = "John";
+pers1.Salut();
+
+int[] numere = new int[5];
+numere[0] = 1;
+int primulNr = numere[0];
+
+//functii
+int Suma(int a, int b)
+{
+    int suma = a + b;
+    return suma;
+}
+
+int rezultat = Suma(5,3);
+
+class Masina 
+{
+    public string marca;
+    public int anFabricatie;
+
+    public void AfiseazaDetalii()
+    {
+        Console.WriteLine("Marca: " + marca); 
+    }
+}
+
+Masina masina1 = new Masina();
+masina1.marca = "BMW";
+masina1.anFabricatie = 2020;
+masina1.AfiseazaDetalii();
+
+// gestionarea exceptiilor
+try {
+    //ceva
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine("A aparut o exceptie: " + ex.Message);
+}
+finally 
+{
+    //bloc care ruleaza intotdeauna, indiferent daca apare o exceptie
+}
+
+class Forma 
+{
+    public virtual void Deseneaza()
+    {
+        Console.writeLine("Des forma");
+    }
+}
+
+class Cerc : Forma
+{
+    public override void Deseneaza()
+    {
+         Console.WriteLine("Desenează un cerc.");
+    }
+}
